5b8f1bcd182b1fbc2c7a468a4c34164c4b9f78d7
Gui Rios <guilhermeriosprog@gmail.com>
2025-10-21 18:00:15 -0300

    feat:criando projeto inicial

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">Sobre</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  border-bottom: 1px solid #ccc;
+  padding: 1rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 16px;
+  text-align: center;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+}
+
+nav a.router-link-exact-active {
+  color: hsla(160, 100%, 37%, 1);
+  font-weight: bold;
+}
+
+main {
+  padding: 1rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Bem-vindo à Página Inicial</h1>
+    <p>Este é o conteúdo da sua home.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.home {
+  padding: 2rem;
+}
+</style>
